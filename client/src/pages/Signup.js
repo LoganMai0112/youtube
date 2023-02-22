761a7817328f128ca51e5aa740f47eb5262678fa
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-02-22 12:34:42 +0700

    feature login logout sign up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,36 +1,53 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useRef, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, redirect, useNavigate } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import axios from 'axios';
 
-function Login() {
+function Signup() {
 	const navigate = useNavigate();
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
+		watch,
 	} = useForm();
+	const password = useRef();
+	const [req, setReq] = useState({ status: true, message: null });
+	password.current = watch('password', '');
 
 	const onSubmit = async (user) => {
 		await axios
-			.post('login', { user })
+			.post('signup', { user })
 			.then(async (res) => {
-				await localStorage.setItem('token', res.headers.get('Authorization'));
-				navigate('/');
+				if (res.data.status.code === 200) {
+					await localStorage.setItem('token', res.headers.get('Authorization'));
+					navigate('/');
+				}
+				if (res.data.status.code === 409) {
+					setReq({ status: false, message: res.data.status.message });
+				}
 			})
 			.catch((err) => console.error(err));
 	};
 	return (
 		<div className="w-full h-screen bg-purple-800 flex justify-center items-center">
 			<div className="max-w-5xl w-full h-fit bg-gray-900 flex p-3 rounded-3xl">
+				<div className="flex-1">
+					<img
+						className="h-full w-full rounded-2xl"
+						src="./Mytube-1.png"
+						alt="signup"
+					/>
+				</div>
 				<div className="flex-1 flex flex-col items-center justify-center">
 					<div className="w-72 flex flex-col">
 						<p className="text-white decoration-2 text-3xl mb-10">
-							Welcome back
+							Create an account
 						</p>
 						<form onSubmit={handleSubmit(onSubmit)}>
 							<input
@@ -44,9 +61,10 @@ function Login() {
 										message: 'Invalid email',
 									},
 								})}
+								onChange={() => setReq({ status: true, message: null })}
 							/>
 							<input
-								className="mb-2 rounded-md border-2 text-md w-full border-main-color bg-sec-color px-2 h-10 focus:outline-none focus:border-main-color placeholder-text-color  text-main-color focus:invalid:border-red-800 focus:invalid:text-red-600"
+								className="mb-6 rounded-md border-2 text-md w-full border-main-color bg-sec-color px-2 h-10 focus:outline-none focus:border-main-color placeholder-text-color  text-main-color focus:invalid:border-red-800 focus:invalid:text-red-600"
 								type="password"
 								placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
 								{...register('password', {
@@ -57,44 +75,49 @@ function Login() {
 									},
 								})}
 							/>
+							<input
+								className="mb-6 rounded-md border-2 text-md w-full border-main-color bg-sec-color px-2 h-10 focus:outline-none focus:border-main-color placeholder-text-color  text-main-color focus:invalid:border-red-800 focus:invalid:text-red-600"
+								type="password"
+								placeholder="Repeat the password"
+								{...register('password_confirmation', {
+									validate: (value) =>
+										value === password.current || 'Passwords do not match',
+								})}
+							/>
+							{req.status === false && (
+								<p className="text-red-700">{req.message}</p>
+							)}
 							{errors.email && (
 								<p className="text-red-700">{errors.email.message}</p>
 							)}
 							{errors.password && (
 								<span className="text-red-700">{errors.password.message}</span>
 							)}
-							<div className="w-full flex justify-end items-center mb-6">
-								<a href className="text-white cursor-pointer hover:underline">
-									Forgot password?
-								</a>
-							</div>
+							{errors.password_confirmation && (
+								<p className="text-red-700">
+									{errors.password_confirmation.message}
+								</p>
+							)}
 							<input
-								className="text-md w-full h-8 bg-main-color rounded-md mb-6 cursor-pointer hover:bg-yellow-300 hover:ring hover:ring-yellow-500"
+								className="mt-4 text-md w-full h-8 bg-main-color rounded-md mb-6 cursor-pointer"
 								type="submit"
-								value="Login"
+								value="Sign up"
 							/>
 						</form>
 						<p className="w-full text-center text-white">
-							Don&apos;t have an account?{' '}
+							Already have an account?{' '}
 							<Link
-								to="/signup"
+								to="/login"
 								className="text-main-color hover:underline cursor-pointer"
 							>
-								Sign up
+								Login
 							</Link>
 						</p>
 					</div>
-				</div>
-				<div className="flex-1">
-					<img
-						className="h-full w-full rounded-2xl"
-						src="./login.jpeg"
-						alt="login"
-					/>
 				</div>
 			</div>
 		</div>
 	);
 }
 
-export default Login;
+export default Signup;
