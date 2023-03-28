92760fbc4dbada3dc4baa8e1ca4778fa06e1fd8d
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-03-28 15:21:01 +0700

    metaprogramming to remove duplicate query

--- server/app/controllers/searchs_controller.rb
+++ server/app/controllers/searchs_controller.rb
@@ -2,26 +2,13 @@ class SearchsController < ApplicationController
   def search
     results = case params[:type]
               when 'video'
-                case params[:date]
-                when 'hour'
-                  VideoSerializer.new(Video.search(params[:query], where: { status: 'published', created_at: { gte: 1.hour.ago } }).includes(:user).to_a, { include: [:user] }).serializable_hash
-                when 'day'
-                  VideoSerializer.new(Video.search(params[:query], where: { status: 'published', created_at: { gte: 1.day.ago } }).includes(:user).to_a, { include: [:user] }).serializable_hash
-                when 'week'
-                  VideoSerializer.new(Video.search(params[:query], where: { status: 'published', created_at: { gte: 1.week.ago } }).includes(:user).to_a, { include: [:user] }).serializable_hash
-                when 'month'
-                  VideoSerializer.new(Video.search(params[:query], where: { status: 'published', created_at: { gte: 1.month.ago } }).includes(:user).to_a, { include: [:user] }).serializable_hash
-                when 'year'
-                  VideoSerializer.new(Video.search(params[:query], where: { status: 'published', created_at: { gte: 1.year.ago } }).includes(:user).to_a, { include: [:user] }).serializable_hash
-                else
-                  VideoSerializer.new(Video.search(params[:query], where: { status: 'published' }).includes(:user).to_a, { include: [:user] }).serializable_hash
-                end
+                VideoSerializer.new(Video.search(params[:query], where: 1.send(params[:date]).ago).includes(:user).to_a, { include: [:user] }).serializable_hash
               when 'channel'
                 UserSerializer.new(User.search(params[:query]).to_a, params: { current_user: current_user }).serializable_hash
               when 'playlist'
                 PlaylistSerializer.new(Playlist.search(params[:query], where: { status: 'published' }).to_a).serializable_hash
               end
-              
+
     if results
       render json: results, status: :ok
     else
