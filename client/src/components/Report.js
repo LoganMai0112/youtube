155506b80e6c1d17b8375465905b9a4a66c6d437
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-03-29 13:15:43 +0700

    report ui for admin delete report

---
+++ client/src/components/Report.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+function Report() {
+  const [reports, setReports] = useState();
+
+  useEffect(() => {
+    const getReports = async () => {
+      await axios
+        .get('/reports')
+        .then((res) => setReports(res.data))
+        .catch((err) => toast(err.response.data.message));
+    };
+
+    getReports();
+  }, []);
+
+  const deleteReport = async (reportId) => {
+    await axios
+      .delete(`/reports/${reportId}`)
+      .then((res) => {
+        toast(res.data.message);
+        setReports(reports.filter((report) => report.id !== reportId));
+      })
+      .catch((err) => toast(err.response.data.message));
+  };
+
+  return (
+    <div className="px-5 flex flex-col gap-2">
+      {reports &&
+        reports.map((report) => (
+          <div className="flex w-full">
+            <Link
+              to={
+                report.reportable_type === 'User'
+                  ? `/users/${report.reportable_id}`
+                  : `/videos/${report.reportable_id}`
+              }
+              className="flex justify-between rounded-l-2xl w-full p-3 bg-sec hover:bg-hover"
+            >
+              <div className="w-72">
+                <p className="text-white">{report.content}</p>
+              </div>
+              <div className="text-text-color">{report.reportable_type}</div>
+            </Link>
+            <button
+              type="button"
+              onClick={() => deleteReport(report.id)}
+              className="bg-sec hover:bg-hover text-text-color hover:text-main-color whitespace-nowrap p-3 rounded-r-2xl"
+            >
+              Delete report
+            </button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default Report;
--- server/app/serializers/user_serializer.rb
+++ server/app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
-  attributes :id, :email, :created_at, :name, :avatar_url, :cover_url
+  attributes :id, :email, :created_at, :name, :avatar_url, :cover_url, :role
 
   attribute :subscribers_count do |object|
     object.subscribers.size
