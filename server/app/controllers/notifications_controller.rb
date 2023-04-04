d5002cc4fb08777503bbd364b37db63dc6f556e0
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-04-05 01:33:10 +0700

    autoformat code by rubocop

--- server/app/controllers/notifications_controller.rb
+++ server/app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   before_action :set_notification, only: :update
-  
+
   def index
     render json: NotificationSerializer.new(current_user.notifications.limit(20).order(created_at: :desc)).serializable_hash, status: :ok
   end
