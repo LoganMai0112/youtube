43f03fcb3a412ecc381c2b8efe0ce41b3bbe8a9c
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-04-05 01:42:00 +0700

    display text when there is no notification

--- server/app/controllers/notifications_controller.rb
+++ server/app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :set_notification, only: :update
 
   def index
-    render json: NotificationSerializer.new(current_user.notifications.limit(20).order(created_at: :desc)).serializable_hash, status: :ok
+    render json: NotificationSerializer.new(current_user.notifications.limit(20).order(created_at: :desc).includes(:notifiable, :user)).serializable_hash, status: :ok
   end
 
   def update
