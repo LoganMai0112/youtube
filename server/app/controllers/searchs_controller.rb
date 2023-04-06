4a75c52c94c5ad2508811db85a15dff692cb05f0
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-04-06 16:09:17 +0700

    optimize query for search

--- server/app/controllers/searchs_controller.rb
+++ server/app/controllers/searchs_controller.rb
@@ -2,20 +2,14 @@ class SearchsController < ApplicationController
   def search
     results = case params[:type]
               when 'video'
-                results = Video.search(params[:query], where: { status: 'published', deleted_at: nil, created_at: { gte: date_params } })
-                video_ids = results.map(&:id)
-                videos = Video.where(id: video_ids).includes(:user).paginate(page: params[:page], per_page: 10)
-                VideoSerializer.new(videos, { include: [:user] }).serializable_hash
+                results = Video.search(params[:query], where: { status: 'published', deleted_at: nil, created_at: { gte: date_params } }, page: params[:page], per_page: 10).includes(:user)
+                VideoSerializer.new(results.to_a, { include: [:user] }).serializable_hash
               when 'channel'
-                results = User.search(params[:query], where: { deleted_at: nil })
-                user_ids = results.map(&:id)
-                users = User.where(id: user_ids).paginate(page: params[:page], per_page: 10)
-                UserSerializer.new(users, params: { current_user: current_user }).serializable_hash
+                results = User.search(params[:query], where: { deleted_at: nil }, page: params[:page], per_page: 10)
+                UserSerializer.new(results.to_a, params: { current_user: current_user }).serializable_hash
               when 'playlist'
-                results = Playlist.search(params[:query], where: { status: 'published' })
-                playlist_ids = results.map(&:id)
-                playlists = Playlist.where(id: playlist_ids).paginate(page: params[:page], per_page: 10)
-                PlaylistSerializer.new(playlists).serializable_hash
+                results = Playlist.search(params[:query], where: { status: 'published' }, page: params[:page], per_page: 10)
+                PlaylistSerializer.new(results.to_a).serializable_hash
               end
 
     if results
