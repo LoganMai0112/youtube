a0460f400c4b8b74d088abdb2746ffc9ce1af05e
LoganMai0112 <logan.mai.goldenowl@gmail.com>
2023-04-07 15:58:41 +0700

    move code in search controller to service

--- server/app/controllers/searchs_controller.rb
+++ server/app/controllers/searchs_controller.rb
@@ -2,20 +2,7 @@ class SearchsController < ApplicationController
   include Pagy::Backend
 
   def search
-    results = case params[:type]
-              when 'video'
-                results = Video.pagy_search(params[:query], where: { status: 'published', deleted_at: nil, created_at: { gte: date_params } })
-                pagy, response = pagy_searchkick(results, items: 10)
-                { data: VideoSerializer.new(response.to_a, { include: [:user] }).serializable_hash, pagy: pagy }
-              when 'channel'
-                results = User.pagy_search(params[:query], where: { deleted_at: nil })
-                pagy, response = pagy_searchkick(results, items: 10)
-                { data: UserSerializer.new(response.to_a, params: { current_user: current_user }).serializable_hash, pagy: pagy }
-              when 'playlist'
-                results = Playlist.pagy_search(params[:query], where: { status: 'published' }, page: params[:page], per_page: 10)
-                pagy, response = pagy_searchkick(results)
-                { data: PlaylistSerializer.new(response.to_a).serializable_hash, pagy: pagy }
-              end
+    results = SearchService.call(params[:type], params[:query], date_params, params[:page], current_user, params: params)
 
     if results
       render json: results, status: :ok
